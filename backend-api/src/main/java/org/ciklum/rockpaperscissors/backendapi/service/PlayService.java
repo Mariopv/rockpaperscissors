9071d8fb1b02ac9e5bcb35d1561a6fde47b3aa2f
Mario Ponce <poncevilches.mario@rindus.de>
2020-05-25 18:09:29 +0200

    backend - tdd first iteration

--- backend-api/src/main/java/org/ciklum/rockpaperscissors/backendapi/service/PlayService.java
+++ backend-api/src/main/java/org/ciklum/rockpaperscissors/backendapi/service/PlayService.java
@@ -1,7 +1,44 @@
 package org.ciklum.rockpaperscissors.backendapi.service;
 
+import org.ciklum.rockpaperscissors.backendapi.model.RoundDto;
+import org.ciklum.rockpaperscissors.backendapi.model.GameResults;
+import org.ciklum.rockpaperscissors.backendapi.model.GameOptions;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PlayService {
+
+    private final StatisticsService statisticsService;
+
+    private final GameOptions firstPlayerOption = GameOptions.ROCK;
+
+    /**
+     * Arg Constructor.
+     *
+     * @param statisticsService the {@link StatisticsService} to set.
+     */
+    @Autowired
+    public PlayService(final StatisticsService statisticsService) {
+        this.statisticsService = statisticsService;
+    }
+
+    public RoundDto getRound() {
+        RoundDto roundDto = playRound();
+        this.statisticsService.updateStatistics(roundDto);
+        return roundDto;
+    }
+
+    RoundDto playRound() {
+        GameOptions secondPlayerOption = GameOptions.ROCK;
+        GameResults gameResult = calculateResult(secondPlayerOption);
+        return new RoundDto(firstPlayerOption,secondPlayerOption,gameResult);
+    }
+
+    GameResults calculateResult(GameOptions secondPlayerOption) {
+        if(secondPlayerOption.equals(GameOptions.ROCK)) {
+            return GameResults.DRAW;
+        }
+        return null;
+    }
 }
--- backend-api/src/test/java/org/ciklum/rockpaperscissors/backendapi/service/PlayServiceTest.java
+++ backend-api/src/test/java/org/ciklum/rockpaperscissors/backendapi/service/PlayServiceTest.java
@@ -1,4 +1,32 @@
 package org.ciklum.rockpaperscissors.backendapi.service;
 
+
+import org.ciklum.rockpaperscissors.backendapi.model.GameOptions;
+import org.ciklum.rockpaperscissors.backendapi.model.GameResults;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
 public class PlayServiceTest {
+
+    @Mock
+    StatisticsService statisticsService;
+
+    PlayService playService;
+
+    @Before
+    public void setup(){
+        playService = new PlayService(statisticsService);
+    }
+
+    @Test
+    public void shouldReturnDraw_WhenSecondPlayerGamesRock() {
+        GameOptions secondPlayerOption = GameOptions.ROCK;
+        Assert.assertEquals(GameResults.DRAW,playService.calculateResult(secondPlayerOption));
+    }
+
 }

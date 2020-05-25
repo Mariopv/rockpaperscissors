61c9687ea18b38b8a25ca762d2d01ca298f685c8
Mario Ponce <poncevilches.mario@rindus.de>
2020-05-25 19:47:38 +0200

    backend - tdd first iteration

--- backend-api/src/main/java/org/ciklum/rockpaperscissors/backendapi/service/StatisticsService.java
+++ backend-api/src/main/java/org/ciklum/rockpaperscissors/backendapi/service/StatisticsService.java
@@ -1,7 +1,43 @@
 package org.ciklum.rockpaperscissors.backendapi.service;
 
+import org.ciklum.rockpaperscissors.backendapi.model.GameResults;
+import org.ciklum.rockpaperscissors.backendapi.model.RoundDto;
 import org.springframework.stereotype.Service;
 
 @Service
 public class StatisticsService {
+
+
+    private int totalRounds;
+    private int totalFirstWin;
+    private int totalSecondWin;
+    private int totalDraw;
+
+    public StatisticsService() {
+        this.totalRounds = 0;
+        this.totalFirstWin = 0;
+        this.totalSecondWin = 0;
+        this.totalDraw = 0;
+    }
+
+    public int getTotalRounds() {
+        return totalRounds;
+    }
+
+    public int getTotalFirstWin() {
+        return totalFirstWin;
+    }
+
+    public int getTotalSecondWin() {
+        return totalSecondWin;
+    }
+
+    public int getTotalDraw() {
+        return totalDraw;
+    }
+
+    public void updateStatistics(RoundDto roundDto) {
+        totalRounds+=1;
+
+    }
 }
--- backend-api/src/test/java/org/ciklum/rockpaperscissors/backendapi/service/StatisticsServiceTest.java
+++ backend-api/src/test/java/org/ciklum/rockpaperscissors/backendapi/service/StatisticsServiceTest.java
@@ -1,4 +1,27 @@
 package org.ciklum.rockpaperscissors.backendapi.service;
 
+import org.ciklum.rockpaperscissors.backendapi.model.GameOptions;
+import org.ciklum.rockpaperscissors.backendapi.model.GameResults;
+import org.ciklum.rockpaperscissors.backendapi.model.RoundDto;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class StatisticsServiceTest {
+
+    StatisticsService statisticsService;
+
+    @Before
+    public void setup(){
+        statisticsService = new StatisticsService();
+    }
+
+    @Test
+    public void shouldIncrementTotalRounds_WhenUpdateStatistics() {
+        RoundDto roundDto = new RoundDto(GameOptions.ROCK,GameOptions.ROCK, GameResults.DRAW);
+        this.statisticsService.updateStatistics(roundDto);
+
+        Assert.assertEquals(this.statisticsService.getTotalRounds(),1);
+    }
+
 }

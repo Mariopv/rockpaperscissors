91df16b9bf641ab8795ec73950a4b785321987ad
Mario Ponce <poncevilches.mario@rindus.de>
2020-05-28 17:34:17 +0200

    frontend - game table tdd second iteration

--- frontend/src/components/gameBoard/gameTable/gameTable.js
+++ frontend/src/components/gameBoard/gameTable/gameTable.js
@@ -1,10 +1,28 @@
 import * as React from 'react'
+import {GameTableRow} from "../gameTableRow/gameTableRow";
 
 class GameTable extends React.Component {
     render() {
-
+        const rounds = this.props.rounds;
+        const rows = [];
+        if(!rounds) {
+            return <table/>
+        }
+        rounds.forEach((round,index) => {
+            rows.push(
+                <GameTableRow key={index} round={round}/>);
+        });
         return (
-            <table/>
+            <table className="table">
+                <thead>
+                <tr>
+                    <th className="column1">First Player</th>
+                    <th className="column2">Second Player</th>
+                    <th className="column3">Result</th>
+                </tr>
+                </thead>
+                <tbody>{rows}</tbody>
+            </table>
         );
     }
 }
--- frontend/src/components/gameBoard/gameTable/gameTable.test.js
+++ frontend/src/components/gameBoard/gameTable/gameTable.test.js
@@ -11,4 +11,20 @@ describe('<GameTable /> tests', () => {
         //Assert
         expect(tree).toMatchSnapshot()
     })
+
+    it("should render table with rounds information", () => {
+        // Arrange
+        let rounds = [{
+            first_player: "ROCK",
+            second_player: 'ROCK',
+            game_result: 'DRAW'
+        }];
+
+        //Act
+        const tree  = renderer.create(<GameTable rounds={rounds}/>).toJSON();
+
+        //Assert
+        expect(tree).toMatchSnapshot();
+
+    })
 })

3bfa1bf065879b9696d22bb7d7e96a98424c9f39
Mario Ponce <poncevilches.mario@rindus.de>
2020-05-28 16:59:21 +0200

    frontend - game table row tdd second iteration

--- frontend/src/components/gameBoard/gameTableRow/gameTableRow.js
+++ frontend/src/components/gameBoard/gameTableRow/gameTableRow.js
@@ -1,7 +1,17 @@
 import * as React from 'react'
 
-function GameTableRow(){
-    return  <tr/>
+function GameTableRow(props){
+    const {round} = props
+    if(!round) {
+        return <tr/>
+    }
+    return (
+        <tr>
+            <td className="column1">{round.first_player}</td>
+            <td className="column2">{round.second_player}</td>
+            <td className="column3">{round.game_result}</td>
+        </tr>
+    );
 }
 
 export { GameTableRow }
--- frontend/src/components/gameBoard/gameTableRow/gameTableRow.test.js
+++ frontend/src/components/gameBoard/gameTableRow/gameTableRow.test.js
@@ -12,4 +12,23 @@ describe('<GameTableRow /> tests', () => {
         expect(tree).toMatchSnapshot()
     })
 
+
+    it("should render row with round information", () => {
+        // Arrange
+        const round = {id:1,
+            first_player: "ROCK",
+            second_player: 'ROCK',
+            game_result: 'DRAW'
+        };
+
+        //Act
+        const tree  = renderer.create(<GameTableRow round={round}/>).toJSON();
+
+        //Assert
+        expect(tree).toMatchSnapshot();
+
+    })
+
+
+
 })

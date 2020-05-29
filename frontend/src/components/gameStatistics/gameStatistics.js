634bc52278d692037c44e301943695a77e326514
Mario Ponce <poncevilches.mario@rindus.de>
2020-05-29 11:40:42 +0200

    frontend - game statistics tdd second iteration

--- frontend/src/components/gameStatistics/gameStatistics.js
+++ frontend/src/components/gameStatistics/gameStatistics.js
@@ -7,9 +7,26 @@ export default class GameStatistics extends React.Component {
             statistics:[]
         }
     }
+
+    setStatistics(statistics) {
+        this.setState({
+            statistics:statistics
+        })
+    }
+
     render() {
+        const statistics = this.state.statistics;
+        const statsRounds = statistics[0];
+        const statsFirstWon = statistics[1];
+        const statsSecondWon = statistics[2];
+        const statsDraw = statistics[3];
         return(
             <div className="game-statistics">
+                <div className="stats-header">Global Statistics Info</div>
+                <div className="totalRounds">Total played rounds:  {statsRounds}</div>
+                <div className="totalFirst">Total 1st player won: {statsFirstWon}</div>
+                <div className="totalSecond">Total 2nd player won: {statsSecondWon}</div>
+                <div className="totalDraw">Total draw rounds:    {statsDraw}</div>
             </div>
         )
     }
--- frontend/src/components/gameStatistics/gameStatistics.test.js
+++ frontend/src/components/gameStatistics/gameStatistics.test.js
@@ -2,6 +2,7 @@ import * as React from 'react'
 import GameStatistics from "./gameStatistics";
 import renderer from "react-test-renderer";
 import './../../config/setupEnzyme'
+import {shallow} from 'enzyme';
 
 describe('<GameStatistics /> tests', () => {
 
@@ -11,6 +12,22 @@ describe('<GameStatistics /> tests', () => {
 
         //Assert
         expect(tree).toMatchSnapshot()
+    })
+
+    it("should render some statistics", () => {
+        // Arrange
+        const component = shallow(<GameStatistics/>)
+        const statistics = [4,3,0,1]
+
+
+        // Act
+        component.instance().setStatistics(statistics);
+        const tree = component.find('.game-statistics');
+
+
+        //Assert
+        expect(tree.find('.totalRounds').props().children[0]).toEqual("Total played rounds:  ");
+        expect(tree.find('.totalRounds').props().children[1]).toEqual(4);
     })
 
 })

6d51307e5b20f49f0d9a2d4472fe019dd335cde0
Mario Ponce <poncevilches.mario@rindus.de>
2020-05-29 11:42:21 +0200

    frontend - styling complete game

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,3 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import GameBoard from "./components/gameBoard/gameBoard";
+import GameStatistics from "./components/gameStatistics/gameStatistics";
+
+ReactDOM.render(
+    <div className="game">
+        <GameBoard/>
+        <GameStatistics/>
+    </div>,
+    document.getElementById('root')
+);
